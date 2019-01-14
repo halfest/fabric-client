d5cf5594a11af5925573f95b1d6482effefbf7f4
Андрей Жданов <>
2019-01-15 00:24:47 +0300

    add logic to ledger client

--- ledger_client.go
+++ ledger_client.go
@@ -1,11 +1,14 @@
 package fabclient
 
 import (
+	"fmt"
+
 	"github.com/hyperledger/fabric-sdk-go/pkg/client/ledger"
+	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/fab"
 )
 
 type LedgerClient struct {
-	Client *ledger.Client
+	client *ledger.Client
 }
 
 func CreateLedgerClient(configPath string, ordererHost string, channelID string, name string, organization string) (*LedgerClient, error) {
@@ -14,4 +17,27 @@ func CreateLedgerClient(configPath string, ordererHost string, channelID string,
 		return nil, err
 	}
 	return fabricClient.CreateLedgerClient(channelID, name, organization)
+}
+
+// GetBlockchainInfoReponse request
+func (l *LedgerClient) GetBlockchainInfoReponse() (*fab.BlockchainInfoResponse, error) {
+	resp, err := l.client.QueryInfo(ledger.WithMaxTargets(3))
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get GetBlockchainInfoReponse\n Error: %v", err)
+	}
+	if resp == nil {
+		return nil, fmt.Errorf("GetBlockchainInfoReponse is nil")
+	}
+	return resp, nil
+}
+
+//GetBlochchainHeight get blockchain height
+func (l *LedgerClient) GetBlochchainHeight() (int, error) {
+	var chainHeight int
+	resp, err := l.GetBlockchainInfoReponse()
+	if err != nil {
+		return chainHeight, err
+	}
+	chainHeight = int(resp.BCI.GetHeight())
+	return chainHeight, nil
 }
